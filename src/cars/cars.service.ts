403991d096f840e52b0722c14ae9b0821cf8c6bb
Jesús Guerrero <jesus.guerrero@talan.com>
2022-08-27 15:55:31 +0200

    Adding interface and UUID to ids

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,20 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { Car } from './car.interface';
 
 @Injectable()
 export class CarsService {
-  private cars = [
+  private cars: Car[] = [
     {
-      id: 1,
+      id: uuid(),
       brand: 'Toyota',
       model: 'Corolla',
     },
     {
-      id: 2,
+      id: uuid(),
       brand: 'Honda',
       model: 'Civic',
     },
     {
-      id: 3,
+      id: uuid(),
       brand: 'Ford',
       model: 'Mustang',
     },
@@ -24,7 +27,7 @@ export class CarsService {
     return this.cars;
   }
 
-  findOneById(id: number) {
+  findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
 
     if (!car) throw new NotFoundException(`Car with '${id}' not found`);
